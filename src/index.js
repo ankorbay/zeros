a73c5ac6cbea26e4384bd7067a5928130c16746f
Anton M Korshunov <anton.m.korshunov@gmail.com>
2018-09-30 13:41:35 +0300

    Zeros Task (#7) solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let counter = 0;
+
+  while (number >= 5) // num of zeros at the end of number(n) is equal to (formula *): n/5+n/5^2+n/5^3+...
+  // instead of calculating the power of 5 each time we may use the loop to devide each previos result by 5. 
+  {
+    number = Math.floor(number / 5); 
+    counter += number;
+  }
+  return counter;
 }
